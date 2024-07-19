20c77bf16f7bd355e93d585a93ddd24a580920c8
Amina <omirzakaminax@gmail.com>
2024-07-19 09:16:47 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -2,14 +2,29 @@ from fastapi import FastAPI
 from app.auth import auth_router
 from app.models import Article
 from app.database import article_collection
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+uri = "mongodb+srv://omirzakaminax:Amina09@prepwhub.gz5kd1v.mongodb.net/?retryWrites=true&w=majority&appName=prepwhub"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 app = FastAPI()
 
 app.include_router(auth_router)
 
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to PrepWHub"}
+
 
 @app.get("/articles")
 async def get_articles():
